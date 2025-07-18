b62231fd2f3a528dbb1deabc17cabd7869ccb908
M.REHAN <rehansiddique962@gmail.com>
2025-07-18 13:11:38 +0500

    final2

--- src/components/navbar_comp.tsx
+++ src/components/navbar_comp.tsx
@@ -18,20 +18,18 @@ import Popupcomp from "./popup_comp";
 import { useState } from "react";
 
 const Navbarcomp = () => {
-  
   const handleShowPopup = () => {
     setShowPopup(true);
   };
-  
-  
+
   const [showPopup, setShowPopup] = useState(false);
   const dispatch = useDispatch();
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   const handleLogout = () => {
     dispatch(setToken(null));
     localStorage.clear();
-    navigate("/login"); 
+    navigate("/login");
   };
   const { userdata } = useSelector((state: RootState) => state.global);
   return (
@@ -41,31 +39,42 @@ const Navbarcomp = () => {
           <img src={logo} alt="" className="h-10 cursor-pointer" />
           <div className="flex items-center gap-3">
             <div
-            className="bg-white/20 p-1.5 rounded-xl  lg:hidden"
-            onClick={handleShowPopup}
-          >
-            <FaPlus size={20} className="text-white"/>
+              className="bg-white/20 p-1.5 rounded-xl  lg:hidden"
+              onClick={handleShowPopup}
+            >
+              <FaPlus size={20} className="text-white" />
+            </div>
+
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <img
+                  src={userdata.profile_picture}
+                  alt=""
+                  className="h-8 w-8 rounded-full cursor-pointer"
+                />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="mt-2 -ml-17">
+                <DropdownMenuCheckboxItem>
+                  <div>
+                    <p className="text-[14px] font-bold text-white">
+                      {userdata.first_name}
+                      {userdata.last_name}
+                    </p>
+                    <p className="text-[12px] text-gray-400">
+                      {userdata.email}
+                    </p>
+                  </div>
+                </DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem
+                  className="text-white hover:text-red-400 cursor-pointer"
+                  onClick={handleLogout}
+                >
+                  <CiLogout className="text-red-400" />
+                  Logout
+                </DropdownMenuCheckboxItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <img
-                src={userdata.profile_picture}
-                alt=""
-                className="h-8 w-8 rounded-full cursor-pointer"
-              />
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-32 mt-2">
-              <DropdownMenuCheckboxItem
-                className="text-white hover:text-red-400 cursor-pointer"
-                onClick={handleLogout}
-              >
-                <CiLogout className="text-red-400" />
-                Logout
-              </DropdownMenuCheckboxItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-          </div>
-          
         </div>
       </MaxWidthWrapper>
       <Separator className="" />
